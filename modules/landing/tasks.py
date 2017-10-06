8e2b17eeb8767592f1d0a17ee7a2db364afa4405
Ricardo <richard020797@gmail.com>
2017-10-05 21:21:01 -0500

    Added default sender

--- modules/landing/tasks.py
+++ modules/landing/tasks.py
@@ -9,6 +9,5 @@ logger = get_task_logger(__name__)
 @periodic_task(run_every=(crontab(minute='*/1')), name="task_send_signup_email", ignore_result=True)
 def task_send_signup_email(email, message):
     """sends an email when singup form is filled successfully"""
-    send_signup_email()
     logger.info("Sent signup email")
     return send_signup_email(email, message)
